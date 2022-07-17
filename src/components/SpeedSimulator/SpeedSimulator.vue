7dc1570efeef5b2937ae3200f4346fb109c9a2d0
PabloAlwaro <salarek15@wp.pl>
2022-07-18 00:52:01 +0200

    speed of light

--- src/components/SpeedSimulator/SpeedSimulator.vue
+++ src/components/SpeedSimulator/SpeedSimulator.vue
@@ -2,11 +2,34 @@
   <div class="speed-simulator">
     <div class="road">
       <div
+        :style="{ position: 'absolute', left: posX2 + '%' }"
+        class="item"
+      ></div>
+    </div>
+    <span class="text"
+      >{{ getDistance(this.velocity2, this.speed2) }} km lub 1 rok
+      świetlny</span
+    >
+    <span class="text">{{ this.velocity2 }} m/s</span>
+    <div class="road">
+      <div
+        :style="{ position: 'absolute', left: posX1 + '%' }"
+        class="item"
+      ></div>
+    </div>
+    <span class="text">{{ getDistance(this.velocity1, this.speed1) }} km</span>
+    <span class="text">{{ getVelocity(this.distance1, this.speed1) }} m/s</span>
+    <div class="road">
+      <div
+        id="dupa"
         :style="{ position: 'absolute', left: posX + '%' }"
         class="item"
       ></div>
     </div>
+    <span class="text">{{ getDistance(this.velocity, this.speed) }} km</span>
+    <span class="text">{{ getVelocity(this.distance, this.speed) }} m/s</span>
     <div class="btn" @click="startGame">Start</div>
+    <div class="btn" @click="stopGame">Stop</div>
     <welcome-screen
       :welcomeScreenStatus="welcomeScreenStatus"
       @setWelcomeStatus="setWelcomeStatus"
@@ -17,20 +40,68 @@
 
 <script>
 import WelcomeScreen from "../WelcomeScreen/WelcomeScreen.vue";
+
 export default {
   components: {
     WelcomeScreen,
   },
   data() {
     return {
+      distance1: 150000000,
+      speed1: 0.2,
+      distance: 384400,
+      velocity: 295988000,
+      velocity1: 300000000,
+      velocity2: 300000000,
+      speed2: 0.00000000315,
+      speed: 77,
+      gameLoopInterval: 0,
+      direction2: "right",
+      direction1: "right",
+      direction: "right",
       play: false,
       posX: 0,
+      posX1: 0,
+      posX2: 0,
+      timer: 0,
       welcomeScreenStatus: false,
       gameStatusText: "Przetestuj prędkości i oddddległości :)",
     };
   },
 
+  watch: {
+    posX(newValue) {
+      if (newValue >= 100) {
+        this.direction = "left";
+      }
+      if (newValue <= 0) {
+        this.direction = "right";
+      }
+    },
+    posX1(newValue) {
+      if (newValue >= 100) {
+        this.direction1 = "left";
+      }
+      if (newValue <= 0) {
+        this.direction1 = "right";
+      }
+    },
+    posX2(newValue) {
+      if (newValue >= 100) {
+        this.direction2 = "left";
+      }
+      if (newValue <= 0) {
+        this.direction2 = "right";
+      }
+    },
+  },
   methods: {
+    getDistance(velocity, speed) {
+      return (velocity / (speed * 100)) * 10;
+    },
+    getVelocity(distance, speed) {
+      return distance * (speed / 100) * 1000;
+    },
     setWelcomeStatus(val) {
       this.welcomeScreenStatus = val;
     },
@@ -38,11 +109,33 @@ export default {
       this.play = true;
       this.gameLoop();
     },
+    stopGame() {
+      this.play = false;
+      clearInterval(this.gameLoopInterval);
+    },
+
     gameLoop() {
       if (this.play) {
-        setInterval(() => {
+        this.gameLoopInterval = setInterval(() => {
           console.log("pizdy");
-          this.posX = this.posX + 10;
+          if (this.direction2 == "right") {
+            this.posX2 = this.posX2 + this.speed2;
+          }
+          if (this.direction2 == "left") {
+            this.posX2 = this.posX2 - this.speed2;
+          }
+          if (this.direction1 == "right") {
+            this.posX1 = this.posX1 + this.speed1;
+          }
+          if (this.direction1 == "left") {
+            this.posX1 = this.posX1 - this.speed1;
+          }
+          if (this.direction == "right") {
+            this.posX = this.posX + this.speed;
+          }
+          if (this.direction == "left") {
+            this.posX = this.posX - this.speed;
+          }
         }, 1000);
       }
     },
@@ -61,6 +154,9 @@ export default {
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  .text {
+    font-size: 30px;
+  }
   .btn {
     cursor: pointer;
     font-size: 1.5em;
@@ -81,11 +177,12 @@ export default {
     position: relative;
     width: 80%;
     background: red;
-    height: 20%;
+    height: 10%;
     .item {
+      transition: 1s linear;
       position: absolute;
       height: 100%;
-      width: 10%;
+      width: 1%;
       background: green;
     }
   }
