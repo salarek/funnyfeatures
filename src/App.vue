eea02ab3de7e447bc74fdacb39eeabb7426d788f
PabloAlwaro <salarek15@wp.pl>
2022-08-14 13:11:26 +0200

    update font

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,13 @@
 </template>
 
 <style>
-@import url("http://fonts.googleapis.com/css?family=Galindo&subset=latin,latin-ext");
+/* @import url("http://fonts.googleapis.com/css?family=Galindo&subset=latin,latin-ext"); */
+@font-face {
+  font-family: "Galindo";
+  src: url("~@/assets/fonts/Galindo-Regular.ttf") format("truetype");
+  font-weight: normal;
+  font-style: normal;
+}
 body {
   margin: 0px;
   padding: 0px;
